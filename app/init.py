ba8d18acd999c4c058defb721cedbddb5c3d246b
lorenzzz <lorenzosaavedra.c@gmail.com>
2024-08-17 16:43:02 -0400

    protorype ADC testing

--- app/init.py
+++ app/init.py
@@ -1,2 +1,22 @@
+import time
+
+import adafruit_ads1x15.ads1115 as ADS
+import board
+import busio
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
 def init_service():
-    print('hola reyes')
+
+    # initialize the I2S interface
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # creating ADS1115 object
+    ads = ADS.ADS1115(i2c)
+
+    # analog input channel
+    channel = AnalogIn(ads, ADS.P0)
+
+    while True:
+        print("AnalogValue: ", channel.value, "Voltage: ", channel.voltage)
+        time.sleep(0.2)
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from app.init import init_service
 
 def main():
     init_service()
+
+
 if __name__ == "__main__":
     main()
